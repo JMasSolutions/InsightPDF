a9a067aaca5a85494d48affb56d1d8b3a3be38a7
Joan Mas Castella <33759606+Joanmascastella@users.noreply.github.com>
2024-11-19 11:39:07 +0800

    Summarization working

--- test_scripts/extractor_test.py
+++ test_scripts/extractor_test.py
@@ -1,6 +1,8 @@
 import pdfplumber
 
 def extract_text_from_pdf(pdf_path):
+    # This function utilizes pdfplumber to extract text from pdfs maintaining the pdf structure.
+    # It returns alls the extracted text which then will be later used to index and summarize.
     text = ''
     try:
         with pdfplumber.open(pdf_path) as pdf:
--- test_scripts/main_test.py
+++ test_scripts/main_test.py
@@ -1,10 +1,22 @@
 from extractor_test import *
+from summarize_test import *
 
 def main():
     #Step 1. Testing if text extraction works
     print("Step 1. Testing Text Extractor")
-    result = extract_text_from_pdf("./files/Recommendation_Report copy.pdf")
-    print(result)
+    extracted_text = extract_text_from_pdf("./files/Recommendation_Report copy.pdf")
+    print(extracted_text)
+
+    print("\n")
+
+    #Step 2. Testing text summarization
+    print("Step 2. Testing Summarization Extractor")
+    summarized_text = summarizer(extracted_text)
+    print(summarized_text)
+
+    print("\n")
+
+    
 
 if __name__ == "__main__":
     main()
