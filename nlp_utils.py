01448adfcb675f662e924f1490b1605162b245eb
Joan Mas Castella <33759606+Joanmascastella@users.noreply.github.com>
2024-11-20 11:38:44 +0800

    refactoring

--- nlp_utils.py
+++ nlp_utils.py
@@ -2,8 +2,6 @@ import spacy
 import subprocess
 import sys
 
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
-
 from env.config import SPACY_MODEL
 
 def download_spacy_model(model_name=SPACY_MODEL):
@@ -13,6 +11,5 @@ def download_spacy_model(model_name=SPACY_MODEL):
         print(f"Spacy model '{model_name}' not found. Downloading it now...")
         subprocess.check_call([sys.executable, "-m", "spacy", "download", model_name])
         print("Download complete.")
-
 
 download_spacy_model()
